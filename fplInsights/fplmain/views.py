fc412d29acb1924676b9531b859d50088e8441b2
chris <chris88herrera@gmail.com>
2022-02-09 21:48:28 -0800

    first index html

--- fplInsights/fplmain/views.py
+++ fplInsights/fplmain/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse   
 
 # Create your views here.
+FPL_INSIGHTS_TEMPLATES_DIR = 'templates/fplInights'
 
 def index(request):
-    return HttpResponse('FIRST RESPONSE')
+    return render(request, f'{FPL_INSIGHTS_TEMPLATES_DIR}/index.html')
